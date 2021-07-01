811ef650954e96695f09b8e4ba37c88747e74c0b
POPOVVITPAV <POPOVVITPAV@yandex.ru>
2021-07-01 05:05:39 +0300

    создал методы

--- src/ru/calculator/Calc.java
+++ src/ru/calculator/Calc.java
@@ -1,6 +1,21 @@
 package ru.calculator;
 
 public class Calc {
-
+	
+	public static void main(String[]args) {
+		
+	}
+	
+	public static int getInt() {
+		
+	} 
+	
+	public static char getOperation() {
+		
+	}
+	
+	public static int calc() {
+		
+	}
 	
 }
