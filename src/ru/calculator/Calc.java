577b5af90eee857b38ee54a1e01233cc11eaeb4c
POPOVVITPAV <POPOVVITPAV@yandex.ru>
2021-07-01 05:51:20 +0300

    Creted function getInt and function getOperation

--- src/ru/calculator/Calc.java
+++ src/ru/calculator/Calc.java
@@ -1,17 +1,30 @@
 package ru.calculator;
 
+import java.util.Scanner;
+
 public class Calc {
-	
+	static Scanner sc = new Scanner(System.in);
 	public static void main(String[]args) {
 		
 	}
 	
 	public static int getInt() {
-		
+		System.out.print("Input integer number");
+		int num;
+		if(sc.hasNextInt()) {
+			num = sc.nextInt();
+		}else {
+			System.out.println("Input only integer number");
+			sc.next();
+			num=getInt();
+		}
+		return num;
 	} 
 	
 	public static char getOperation() {
-		
+		System.out.print("Input operation +,-,*,/"){
+			
+		}
 	}
 	
 	public static int calc() {
